79a0a328692db0c90c1dcdd7fdf0dc49e432c9c3
ParthKharbanda2003 <PARTHKHARBANDA111.PK@GMAIL.COM>
2024-07-24 02:05:16 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function App() {
   return (
-    <h1 className='bg-red-500'>Hello World!</h1>
+    <h1 className='bg-black text-purple-800'>Hello World!</h1>
   )
 }
