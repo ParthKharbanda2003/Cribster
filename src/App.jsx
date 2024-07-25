9fe396fc71502fdb4420b71efa8538ff1236995c
ParthKharbanda2003 <PARTHKHARBANDA111.PK@GMAIL.COM>
2024-07-26 00:37:52 +0530

    routes done

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import SignIn from './pages/SignIn.jsx'
+import Header from './components/Header.jsx'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
+    <Header/>
+    <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/about" element={<About />} />
-          <Route path="/profle" element={<Profile />} />
-
+          <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   )
