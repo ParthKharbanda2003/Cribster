0551a9c68c469a581b2e808e19e0e701ba923e21
ParthKharbanda2003 <PARTHKHARBANDA111.PK@GMAIL.COM>
2024-07-28 01:19:01 +0530

    connect db

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
+// import connectDB from '../database/mongodb.js';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('con to mongodb!');
+    }).catch((err) => {
+        console.log(err);
+    })
 
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-}
-);
