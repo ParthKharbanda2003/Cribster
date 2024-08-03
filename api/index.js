e574de9f9b1ca9aa6b73e34b638e63445172dc7a
ParthKharbanda2003 <PARTHKHARBANDA111.PK@GMAIL.COM>
2024-08-03 14:01:54 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log('con to mongodb!');
+    console.log('Welcome to mongodb!');
     }).catch((err) => {
         console.log(err);
     })
