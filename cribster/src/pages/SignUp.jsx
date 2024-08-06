160a153c0243124f0a138f040f3213984faf20b0
ParthKharbanda2003 <PARTHKHARBANDA111.PK@GMAIL.COM>
2024-08-06 23:47:01 +0530

    .

--- cribster/src/pages/SignUp.jsx
+++ cribster/src/pages/SignUp.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 
 export default function SignUp() {
+  const [formData, setFormData]= useState({})
+
+  const handleChange = (e) => {
+    setFormData({
+      
+    })
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form className='flex flex-col gap-4 '>
 
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
+        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username'  onChange={handleChange} />
 
-        <input type="text" placeholder='email' className='border p-3 rounded-lg' id='email' />
+        <input type="text" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
 
-        <input type="text" placeholder='password' className='border p-3 rounded-lg' id='password' />
+        <input type="text" placeholder='password' className='border p-3 rounded-lg' id='password'  onChange={handleChange}/>
 
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:80'>Sign Up</button>
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:80' onChange={handleChange}>Sign Up</button>
 
       </form>
 
